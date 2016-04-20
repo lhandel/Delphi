a5e1eb74017d44a7b544c70a0ad7789d39c06044
Ludwig <ludwighandel@gmail.com>
2016-04-20 08:41:07 +0200

    First commit

---
+++ instore/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/style.css" media="screen" charset="utf-8">
+    <title>Instore</title>
+  </head>
+  <body>
+
+    <div class="container">
+
+        <div class="phone_no">
+
+            <form action="index.php" method="post">
+                <input type="number" name="number" value="" placeholder="Your phonenumber">
+                <input type="submit" name="name" value="Done!">
+            </form>
+
+        </div>
+
+        <div class="phone_no2"></div>
+        
+    </div>
+
+  </body>
+</html>
