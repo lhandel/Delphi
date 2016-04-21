325cd375d8b6b7f4f9785bb7431c0e0413e58797
Ludwig <ludwighandel@gmail.com>
2016-04-21 14:10:27 +0200

    A new design

--- instore/index.php
+++ instore/index.php
@@ -2,25 +2,25 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/style.css" media="screen" charset="utf-8">
     <title>Instore</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="assets/style.css" media="screen" title="no title" charset="utf-8">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
   </head>
   <body>
-
     <div class="container">
+        <h1>What do you need<br/> help with?</h1>
 
-        <div class="phone_no">
+        <div class="btn_container">
 
-            <form action="index.php" method="post">
-                <input type="number" name="number" value="" placeholder="Your phonenumber">
-                <input type="submit" name="name" value="Done!">
-            </form>
+            <ul class="btn_list">
+              <li><a href="#">Help desk</a></li>
+              <li><a href="#">Recipes</a></li>
+            </ul>
 
         </div>
 
-        <div class="phone_no2"></div>
-        
     </div>
-
+    <div class="logo"></div>
   </body>
 </html>
