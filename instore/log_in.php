2f61d1f11f285fbad7d3ec7924369d2ff18324c1
evchen <minjia@kth.se>
2016-04-22 09:10:31 +0200

    Added files via upload

---
+++ instore/log_in.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Instore</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="assets/style.css" media="screen" title="no title" charset="utf-8">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div class="container">
+        <input id="back_button" type="submit" value=""  >
+        <h3>Help Desk</h3>
+        <h2>Enter your MOBILE number below to enter the queue</h2>
+
+        <div class="log_in">
+
+          <input id="number" type="number" placeholder="Enter mobile number">
+          <input id="submit_button" type="submit" value="Register">
+
+        </div>
+        <div class="login_display">
+          <div class="queue_icon">
+          </div>
+          <div class="time_icon">
+          </div>
+          <div class="info">
+            <p class="important">5</p>
+            <p class="info">people <br/>in front</p>
+          </div>
+          <div class="info">
+            <p class="important">23</p>
+            <p class="info">minutes <br/>left</p>
+          </div>
+        </div>
+
+    </div>
+    <div class="logo"></div>
+  </body>
+</html>
