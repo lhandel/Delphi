946918b4a8ecf109275da562951f7a147ab91bb6
KarasMI7 <stevenzhou@n158-p200.kthopen.kth.se>
2016-04-22 09:40:43 +0200

    Clock animation code
    Clock animation code for more feature

---
+++ Clock Animation/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <script>
+        alert('Welcome to Delphi!');
+      </script>
+    <?php
+      echo 'IN DEVELOPMENT!';
+     ?>
+     <div class="clock">
+       <div class="top"></div>
+       <div class="right"></div>
+       <div class="bottom"></div>
+       <div class="left"></div>
+       <div class="center"></div>
+       <div class="shadow"></div>
+       <div class="hour"></div>
+       <div class="minute"></div>
+     </div>
+     <h1> Virtual Q System </h1>
+     <h2> Sprint 1 Edition </h2>
+  </body>
+
+</html>
