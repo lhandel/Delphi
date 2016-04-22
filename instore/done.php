a4fbb1248aa2daa1a905e3a95ae282dac50bae22
Juzov <ejuzov@gmail.com>
2016-04-22 08:47:23 +0200

    done page

---
+++ instore/done.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Instore</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="assets/style.css" media="screen" title="no title" charset="utf-8">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <h2>Done <i class="fa fa-check-circle ml" aria-hidden="true"></i></h2>
+        <h4>Here's your number</h4>
+      </div>
+      <div class="row">
+        <div class="icon">
+          <img src="assets/test.png" alt="" / class="ticket">
+
+        </div>
+      </div>
+
+      <div class="row">
+        <p>
+          A SMS Has Been Sent To
+        </p>
+
+        <h3>
+          +46 735 434 280
+        </h3>
+
+        <div class="p2">
+          SMS Can Take Up To 30s To Be Recieved
+        </div>
+
+      </div>
+      <div class="row">
+
+      </div>
+      <div class="row">
+        <a href="#" class="btn">Finish</a>
+      </div>
+    </div>
+
+
+  </body>
+</html>
