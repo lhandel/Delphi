1e002a32ff4d501eba2a520bf933615a19746c64
Brunnner <thomas.brunner@live.se>
2016-04-26 13:45:08 +0200

    Admin dashboard

---
+++ admin/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="assets/style.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="s1">
+
+      <div class="row">
+          <a href="#" class="btn"> <h1> Service 1 <h1> </a>
+      </div>
+        <div class="row">
+          <div class="infoc">
+              <div class="est">
+                <h1 class="stat" >22</h1> <br>
+                <h2>est. time</h2>
+
+              </div>
+              <div class="handler">
+                <h1 class="stat" >
+                  2
+                </h1>
+              </div>
+              <div class="queue">
+                <h1 class="stat" >
+                  2
+                </h1>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
