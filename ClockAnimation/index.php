1a010717dbf24f8da6d5af4d6ffe57ff64d40329
KarasMI7 <steven.zhou930605@gmail.com>
2016-04-26 17:01:33 +0200

    clock animation

---
+++ ClockAnimation/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>index</title>
+    <!--...<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">...-->
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+    <link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
+
+  </head>
+  <body>
+    <div class="clock">
+       <div class="top"></div>
+       <div class="right"></div>
+       <div class="bottom"></div>
+       <div class="left"></div>
+       <div class="center"></div>
+       <div class="shadow"></div>
+       <div class="hour"></div>
+       <div class="minute"></div>
+     </div>
+  </body>
+</html>
