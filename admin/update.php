b8d2044efbb43ae0b75591a205a7894a4bb2b7ff
Ludwig <ludwighandel@gmail.com>
2016-04-28 14:17:33 +0200

    Update for admin

--- admin/update.php
+++ admin/update.php
@@ -4,15 +4,17 @@ include '../function.php';
 
   if(isset($_GET["next"]))
   {
-    $s_id= $_GET["s_id"];
-    $result = user_update_by_service($s_id);
+    $s_id = $_GET["s_id"];
+    $a_id = 1;
+    $result = user_update_by_service($s_id,$a_id);
     header("Location: service.php?s_id=".$s_id);
   }
 
   if(isset($_GET["skip"]))
   {
     $s_id= $_GET["s_id"];
+    $a_id = 1;
     user_update_state();
-    user_update_by_service($s_id);
+    user_update_by_service($s_id,$a_id);
     header("Location: service.php?s_id=".$s_id);
   }
