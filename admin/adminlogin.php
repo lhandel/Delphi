d1ca3e8a593890d0a339b02ea793833f5112518d
KarasMI7 <steven.zhou930605@gmail.com>
2016-04-28 14:22:00 +0200

    Admin Login Page

---
+++ admin/adminlogin.php
@@ -0,0 +1,28 @@
+<?php include '../function.php'; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Login</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="assets/style.css" media="screen" title="no title" charset="utf-8">
+
+  </head>
+  <body>
+
+    <div class="container">
+
+      <div class="loginblock">
+        <h1 class="header"> <img src="assets/Admin.svg" class="icon" />Admin Login</h1>
+
+        <input id="adminname" type="text" name="adminname" value="">
+        <input id="login_button" type="button" name="login_button" value="LOG IN">
+
+      </div>
+        <img src="assets/logo.svg" class="logo">
+    </div>
+
+  </body>
+</html>
