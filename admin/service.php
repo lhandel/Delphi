b9fe2c904adb745d332b7fb6cfbf77f4685577bd
Ludwig <ludwighandel@gmail.com>
2016-04-29 11:56:57 +0200

    Improved service page

--- admin/service.php
+++ admin/service.php
@@ -1,101 +1,90 @@
 <?php include '../function.php'; ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Admin service</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<head>
+  <meta charset="utf-8">
+  <title>Admin service</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" charset="utf-8"></script>
-    <script src="assets/script.js" charset="utf-8"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="assets/style.css" media="screen" title="no title" charset="utf-8">
-  </head>
-  <body>
-    <?php
-    include 'menu.php';
-    $service = get_service($_GET['s_id']);
-     ?>
-    <div class="container">
-      <div class="row">
-        <div class="service">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" charset="utf-8"></script>
+  <script src="assets/script.js" charset="utf-8"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="assets/style.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+<?php
+  include 'menu.php';
+  $service = get_service($_GET['s_id']);
+?>
+<div class="container">
 
-      <div class="row">
-        <div class="title mg">
-          <h1> <?php echo $service['name']; ?> <h1>
-        </div>
+      <div class="row service">
 
-      </div>
           <div class="row">
-            <div class="infoc">
-              <div class="row">
-                <div class="box_1">
-
-                  <div class="est test2">
-                    <div class="icon">
-                      <i class="fa fa-clock-o" aria-hidden="true"></i>
-                    </div>
-                    <h1 class="stat" >22</h1>
-                    <h2>est. time</h2>
-                  </div>
-
-                  <div class="handler test2">
-                    <img class = "icon" src="assets/Admin.svg" alt="" />
-
-                    <h1 class="stat" >
-                      <?php echo $service['handler']; ?>
-                    </h1>
-                    <h2>Handlers</h2>
-
-                  </div>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="box_2">
-
-                  <div class="est new">
-                    <div class="icon">
-                      <i class="fa fa-clock-o" aria-hidden="true"></i>
-                    </div>
-                    <h1 class="stat" ><?php echo $service['current']; ?></h1>
-                    <h2>Current number</h2>
-                  </div>
-
-                  <div class="queue new">
-                    <img class = "icon" src="assets/QueueGrey.svg" alt="" />
-                    <h1 class="stat" ><?php echo $service['queue_count']; ?></h1>
-                    <h2>Queue</h2>
-                  </div>
-                </div>
-
-              </div>
+            <div class="title mg">
+              <h1> <?php echo $service['name']; ?> <h1>
             </div>
           </div>
-        </div>
+
+          <div class="row">
+
+            <div class="box_container">
+
+              <div class="box first">
+                <div class="icon">
+                  <i class="fa fa-clock-o" aria-hidden="true"></i>
+                </div>
+                <div class="number">--</div>
+                <h2>est. time</h2>
+              </div>
+              <div class="box first last">
+                <div class="icon">
+                  <img class = "icon" src="assets/Admin.svg" alt="" />
+                </div>
+                <div class="number"><?php echo $service['handler']; ?></div>
+                <h2>Handlers</h2>
+              </div>
+
+
+              <div class="box">
+                <div class="icon">
+                  <i class="fa fa-clock-o" aria-hidden="true"></i>
+                </div>
+                <div class="number"><?php echo $service['current']; ?></div>
+                <h2>Current number</h2>
+              </div>
+
+              <div class="box last">
+                <div class="icon">
+                  <img class = "icon" src="assets/QueueGrey.svg" alt="" />
+                </div>
+                <div class="number"><?php echo $service['queue_count']; ?></div>
+                <h2>Queue</h2>
+              </div>
+
+
+            </div><!-- .endboxcontainer -->
+          </div><!-- .end row -->
+      </div><!-- .end row & servicebox -->
 
       <div class="row">
-        <div class="b_1">
-          <a href="update.php?s_id=<?php echo $_GET["s_id"];?>&next=true" class="btn_1">
-            <h1>New customer</h1>
-          </a>
-        </div>
+          <a href="update.php?s_id=<?php echo $_GET["s_id"];?>&next=true" class="button large">Next</a>
       </div>
 
       <div class="row">
-        <div class="b_1">
-          <a href="update.php?s_id=<?php echo $_GET["s_id"];?>&skip=true" class="btn_1"> <h1>Skip customer</h1></a>
+        <div class="col-2">
+            <a href="index.php" class="button">Change service</a>
+        </div>
+        <div class="col-2 last">
+            <a href="update.php?s_id=<?php echo $_GET["s_id"];?>&skip=true" class="button">Skip customer</a>
         </div>
       </div>
 
-      <div class="row">
-        <div class="b_1">
-          <a href="index.php" class="btn_1"> <h1>Change service</h1></a>
-        </div>
-      </div>
 
-    </body>
+</div>
+
+</body>
 
 </html>
