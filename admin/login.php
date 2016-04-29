4eb6374588ca6e004641cc1d41ab2a6026db344e
Ludwig <ludwighandel@gmail.com>
2016-04-29 14:59:21 +0200

    Fixed menu for admin login

--- admin/login.php
+++ admin/login.php
@@ -23,14 +23,17 @@ if(isset($_POST['a_id'])){
   <head>
     <meta charset="utf-8">
     <title>Admin Login</title>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" charset="utf-8"></script>
+    <script src="assets/script.js" charset="utf-8"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="assets/style.css" media="screen" title="no title" charset="utf-8">
 
   </head>
   <body>
-
+    <?php
+    include 'menu.php';
+     ?>
     <div class="container">
 
       <div class="loginblock">
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <?php
 session_start();
+
 if(isset($_SESSION["c_id"])){
   header("Location: ia.php");
 }
