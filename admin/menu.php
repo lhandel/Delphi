2856ee5019e86b8c86712a5f08977dc9a3dda44c
KarasMI7 <steven.zhou930605@gmail.com>
2016-05-11 16:30:34 +0200

    fix 2 for logo

--- admin/menu.php
+++ admin/menu.php
@@ -2,10 +2,11 @@
   <i class="fa fa-bars" aria-hidden="true"></i>
 </div>
 <div class="menu">
-  <a href="index.php"><i class="fa fa-tachometer" aria-hidden="true"></i><img src="assets/logo.svg" alt="" /></a>
+  <a href="index.php"><img src="assets/logo.svg" alt="" /></a>
   <ul>
     <li><a href="index.php" id="item1"><i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</a></li>
     <li><a href="statistic.php" id="item2"><i class="fa fa-bar-chart" aria-hidden="true"></i> Statistic</a></li>
     <li><a href="settings.php" id="item3"><i class="fa fa-wrench" aria-hidden="true"></i> Settings</a></li>
   </ul>
+
 </div>
