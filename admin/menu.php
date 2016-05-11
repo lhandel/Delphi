5354fdafd34ff5e815f8f085df110189d04fd62a
KarasMI7 <steven.zhou930605@gmail.com>
2016-05-11 15:38:12 +0200

    small update on DQ logo
    when pressed DQ logo in admin or settings, it redirect you to the
    index.php

--- admin/menu.php
+++ admin/menu.php
@@ -2,7 +2,7 @@
   <i class="fa fa-bars" aria-hidden="true"></i>
 </div>
 <div class="menu">
-  <img src="assets/logo.svg" alt="" />
+  <a href="index.php"><i class="fa fa-tachometer" aria-hidden="true"></i><img src="assets/logo.svg" alt="" /></a>
   <ul>
     <li><a href="index.php" id="item1"><i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</a></li>
     <li><a href="statistic.php" id="item2"><i class="fa fa-bar-chart" aria-hidden="true"></i> Statistic</a></li>
