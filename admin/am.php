9c2aa01e1d00086d69cd04806fe7b0ac05e65c14
Brunnner <thomas.brunner@live.se>
2016-05-12 14:18:52 +0200

    Admin managment beta

--- admin/am.php
+++ admin/am.php
@@ -30,22 +30,21 @@
               <tr>
                 <td class="ttitle tmain">
                   <?php  echo $row['admin_name'];?>
-                  <div class="annotation">
-                    Name of service. Click will get you to service
-                  </div>
                 </td>
 <!-- Testar härifrån-->
                 <td class="ttitle ticons">
 
                   <a href="#" class="ta" onclick="popup_s('<?php echo 'rem';?>',<?php echo $row['s_id'];?>)"><!-- ändra länk till popup rutan-->
-                  
 
+                  <?php echo $row['a_id']; ?>
                   </a>
 
-<!-- hit -->
                   <div class="annotation">
-                    Change reminder time
+                    Admin id
                   </div>
+
+<!-- hit -->
+
                 </td>
 
                 <td class="ttitle ticons">
@@ -54,27 +53,20 @@
                   </a>
 
                   <div class="annotation">
-                    Click to edit name of service
+                    Click to edit name of admin
                   </div>
                 </td>
 
 
 
-                <td class="ttitle ticons">
-                  <a href="update.php?s_id=<?php echo $row["s_id"];?>&reset=true" class="ta">
-                    <i class="fa fa-recycle" aria-hidden="true"></i>
-                  </a>
-                  <div class="annotation">
-                    Click to reset queue
-                  </div>
-                </td>
+
                 <td class="ttitle ticons">
 
                   <a href="update.php?s_id=<?php echo $row["s_id"];?>&remove=true" class="ta">
                     <i class="fa fa-trash-o" aria-hidden="true"></i>
                   </a>
                   <div class="annotation">
-                    Click to remove service
+                    Click to remove admin
                   </div>
                 </td>
               </tr>
@@ -92,6 +84,36 @@
               </div>
           </div>
     </div>
+
+    <div class="row">
+      <div class="s1 ns">
+
+        <div class="row">
+          <div class="nsinfo">
+            <h1 class="nh2">
+              New Admin
+            </h1>
+            <h2 class="nh2"> Fill Out New Admin Name</h2>
+          </div>
+        </div class="row">
+
+        <div class="row">
+          <form class="formErase" action="update.php" method="post" id="createNS">
+            <input class="infoc" type="text" name="name" value="">
+          </form>
+        </div class="row">
+
+
+        <div class="row">
+          <button type="submit" form="createNS" value="Submit" class="btn servicebtn cns">  Create <i class="fa fa-angle-right" aria-hidden="true"></i> </button>
+        </div>
+
+      </div>
+
+    </div class="row">
+
+  </div>
+
 
   </body>
 </html>
