408e3bd8875dcddf85a4bf0ea799c1e7837de0c0
Ludwig <ludwighandel@gmail.com>
2016-05-13 08:56:27 +0200

    some bug fix 🐞

---
+++ admin/statistic.php
@@ -0,0 +1,23 @@
+<?php include '../function.php';  protect("company");  ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Management</title>
+  </head>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" charset="utf-8"></script>
+  <script src="assets/script.js" charset="utf-8"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="assets/style.css" media="screen" title="no title" charset="utf-8">
+
+  <body>
+    <?php
+      include 'menu.php';
+    ?>
+
+    
+
+
+  </body>
+</html>
