3f1b0328f95cd811198e5241ae42336f765bad4d
evchen <minjia@kth.se>
2016-05-16 12:48:27 +0200

    css added

--- Delphi2/application/views/instore/index_i.php
+++ Delphi2/application/views/instore/index_i.php
@@ -8,7 +8,7 @@
 
     <meta name="apple-mobile-web-app-capable" content="yes">
 
-    <link rel="stylesheet" href=<?php echo base_url('assets/instore/style.css');?> media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href=<?php echo base_url('assets/instore/style.css') ?> media="screen" title="no title" charset="utf-8">
     <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
   </head>
@@ -30,7 +30,7 @@
 
     </div>
 
-    <img src="assets/logo.svg" class="logo">
+    <img src="<?php echo base_url('assets/instore/logo.svg') ?>" alt="" class="logo">
 
   </body>
 </html>
---
+++ Delphi2/assets/user/script.js
@@ -0,0 +1,66 @@
+var temp = 0;
+var ewt = 1;
+var temp_time= new Date().getMinutes();
+$(document).ready(function(){
+
+  countdown();
+
+  function pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+  }
+  function countdown(){
+
+    var dataString = {
+      public_id : public_id
+    };
+
+        $.ajax({
+        url: 'ewt.php',
+        data:  dataString,
+        cache: false,
+        success:function(result){
+          var current_time = new Date().getMinutes();
+//          $("#ewt_user").html(result.flag);
+
+
+          if(result.state == 1){ // if the user is called to the queue his state is 1
+            $("#ewt_user").html("It's your turn");
+
+          }
+          else if(result.state == 4){
+            $("#ewr_user").html("You got removed from the queue")
+          }
+
+          else if(result.flag ==1||result.content==0){
+            $("#display_minute").hide();
+            $("#display_ewt").hide();
+            $("#clock_icon").hide();
+            $("p").css({"font-weight":"bold", "font-size":"25px"});
+            $("#ewt_user").html("It will soon be your turn </br>please return to the store");
+            $("#in_queue").html(result.inline);
+          }
+
+
+          else {
+            $("#in_queue").html(result.inline);
+            $("#ewt_user").html(result.content);
+          }
+
+        }
+    });
+  }
+  setInterval(countdown,1000);
+
+  function blink() {
+      $("#ewt").fadeTo(100, 0.1).fadeTo(200, 1.0);
+    }
+
+
+
+
+
+
+
+
+
+})
