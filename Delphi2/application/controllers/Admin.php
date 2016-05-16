3f6c7d98750852b11075e455d489e18620a63ae3
Ludwig <ludwighandel@gmail.com>
2016-05-16 16:31:03 +0200

    some fixes

--- Delphi2/application/controllers/Admin.php
+++ Delphi2/application/controllers/Admin.php
@@ -6,63 +6,59 @@ class Admin extends CI_Controller {
 	public function index()
 	{
 		//
+
 		$this->listService();
 
 	}
 	//check session a_id
 	public function checkLogin(){
 
-		//Load model
-		$this->load->model('admin_m');
-
-		$sess=$this->session->userdata('a_id');
-		// echo var_dump($sess);
-		// echo var_dump($_GET['url']);
-		// echo var_dump($_GET['s_id']);
-
-		if($sess){
+		if($this->session->userdata('a_id')){
 		  if (isset($_GET['url'])) {
 		    redirect(site_url("index.php/admin/".$_GET['url']));
 		  }else {
-		    $s_id= $_GET['s_id'];
-				redirect(site_url("index.php/admin/service.php?$s_id"));
+				redirect(site_url("index.php/admin/service.php?s_id=".$_GET['s_id']));
 		  }
 		} else {
-			$this->login();
+				$url = (isset($_GET['url']))? "?url=".$_GET['url'] : '';
+				header("Location: ".site_url("index.php/admin/login".$url));
 		}
 }
 public function login(){
-		// redirect(site_url("index.php/admin/login"));
 
+
+	//session a_id is typed
+	if(isset($_POST['a_id'])){
+
+		$a_id = intval(trim($_POST['a_id']));
+		// Check in db if adminname exist
+
+		//Load model
 		$this->load->model('admin_m');
-		//session a_id is typed
-		if(isset($_POST['a_id'])){
-			$a_id = trim($_POST['a_id']);
-			// Check in db if adminname exist
-			$a_id = intval($a_id);
-			//call model
 
-			$result=$this->admin_m->check_admin_id($a_id);
+		$result=$this->admin_m->check_admin_id($a_id);
 
-			if ($result != 0 ) {	//admin does not exist
-				$this->session->set_userdata('a_id',$a_id);
-				if (isset($_GET['url'])) {
-					echo var_dump($_GET['url']);
-					redirect(site_url("index.php/admin/".$_GET['url']));
-				}else {
-					$s_id= $_GET['s_id'];
-					redirect(site_url("index.php/admin/service.php?s_id=".$s_id));
-				}
-			}else	redirect(site_url("index.php/admin/login.php?$s_id&wrong=true"));
-		}
-		$this->load->view('admin/login');
+		if ($result != 0 ) {	//admin does not exist
+			$this->session->set_userdata('a_id',$a_id);
+			if (isset($_GET['url'])) {
+				echo var_dump($_GET['url']);
+				redirect(site_url("index.php/admin/".$_GET['url']));
+			}else {
+				$s_id= $_GET['s_id'];
+				redirect(site_url("index.php/admin/service.php?s_id=".$s_id));
+			}
+		}else	redirect(site_url("index.php/admin/login.php?$s_id&wrong=true"));
+	}
+	
+	$this->load->view('admin/login');
+
 }
 
 /* All services in the admin dashboard */
 	public function listService(){
 		// Load the model
 		$this->load->model('service_m');
-
+				$this->checkLogin();
 		// Get the serivies
 		$data['services']  = $this->service_m->getServices(1);  //  change to session!!!!
 
