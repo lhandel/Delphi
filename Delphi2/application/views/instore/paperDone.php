708d58c947e78edd45a5bf4ef10a3311150d87a2
gulinds <Gustav_salem@hotmail.com>
2016-05-19 09:19:38 +0200

    paperdone

--- Delphi2/application/views/instore/paperDone.php
+++ Delphi2/application/views/instore/paperDone.php
@@ -20,7 +20,7 @@
   </head>
   <body  <?php echo $theme; ?>>
     <div class="container">
-      <h2 style="margin-top:60px; font-size: 50px;">Grab Your <br>Ticket <br><b><h1 style="font-size: 60px; margin-top: 10px;">Here!</h1></b></h2>
+      <h2 style="margin-top:150px; font-size: 50px;">Grab Your <br>Ticket <br><b><h1 style="font-size: 60px; margin-top: 10px;">Here!</h1></b></h2>
       <div class="icon-arrowdown">
         <i class="fa fa-arrow-down" aria-hidden="true"></i>
       </div>
