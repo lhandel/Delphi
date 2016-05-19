dcfdef83a742b3e9748c223da23469a45f884846
KarasMI7 <steven.zhou930605@gmail.com>
2016-05-19 09:26:22 +0200

    Theme for IA fixed

--- Delphi2/application/views/ia.php
+++ Delphi2/application/views/ia.php
@@ -8,11 +8,12 @@
 
     <meta name="apple-mobile-web-app-capable" content="yes">
 
+    <link rel="stylesheet" href="<?php echo site_url('assets/templates.css'); ?>" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href=<?php echo base_url('assets/main.css') ?> media="screen" title="no title" charset="utf-8">
     <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,700,400|Open+Sans:400,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
   </head>
-  <body>
+  <body <?php echo $theme; ?> >
     <div class="container">
         <h1 class="startpage">Welcome to DQ</h1>
         <h3 class="choosemenu">Select Menu</h3>
